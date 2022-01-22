3131e546097f54de163e82a6eb1ef859e5587407
Derek B <derekbutvin@gmail.com>
2022-01-22 13:19:44 -0600

    Created bare-bones chatrooms and sessions controllers

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,22 @@
+class ChatroomsController < ApplicationController
+
+    def index
+        # @chatrooms = Chatroom.all
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def delete
+    end
+
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import '@doabit/semantic-ui-sass'
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
